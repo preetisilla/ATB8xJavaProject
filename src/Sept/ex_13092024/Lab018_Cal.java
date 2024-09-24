f10f64eed9ef81fd83ebc007a06713611edaf284
preetisilla <preetisilla18@gmail.com>
2024-09-24 18:45:44 +0530

    New Project

---
+++ src/Sept/ex_13092024/Lab018_Cal.java
@@ -0,0 +1,16 @@
+package Sept.ex_13092024;
+
+public class Lab018_Cal {
+    public static void main(String[] args) {
+        int a = 34;
+        int b = 10;
+        System.out.printf("a+b = %d",a+b);
+        System.out.println();
+        System.out.printf("a-b = %d",a-b);
+        System.out.println();
+        System.out.printf("a*b = %d",a*b);
+        System.out.println();
+        System.out.printf("a/b = %d",a/b);
+
+    }
+}
