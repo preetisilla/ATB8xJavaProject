a97198c921de0ac10ad5bea5df3ccbeb47c167bd
preetisilla <preetisilla18@gmail.com>
2024-09-24 17:49:04 +0530

    New Project

---
+++ src/Sept/ex_13092024/Lab015.java
@@ -0,0 +1,9 @@
+package Sept.ex_13092024;
+
+public class Lab015 {
+    public static void main(String[] args) {
+        String name = "Preeti";
+        String lastName = "Silla";
+        System.out.printf("My name is %s and Last name is %s",name,lastName);
+    }
+}
