f4bce4f74c105b1e763eed87f96d5f3be17e03a1
preetisilla <preetisilla18@gmail.com>
2024-09-24 18:38:18 +0530

    New Project

---
+++ src/Sept/ex_13092024/Lab017.java
@@ -0,0 +1,13 @@
+package Sept.ex_13092024;
+
+public class Lab017 {
+    public static void main(String[] args) {
+        //Constant
+        //constants are variables
+        //values cannot be changed once assigned
+        //final datatype variable name = variable value
+        final float PIE;
+        PIE = 3.14f;
+        System.out.println(PIE);
+    }
+}
