de41e80de75b6d0e9f3db1b175dcc5015ae80a29
preetisilla <preetisilla18@gmail.com>
2024-09-24 17:45:33 +0530

    New Project

---
+++ src/Sept/ex_13092024/Lab013.java
@@ -0,0 +1,11 @@
+package Sept.ex_13092024;
+
+public class Lab013 {
+    public static void main(String[] args) {
+        //diff btw print and println
+        //println is for new line after string print
+        System.out.println("Hello");
+        System.out.print("World");
+        System.out.println("Hi All");
+    }
+}
