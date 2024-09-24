580c74cc2b9afc871d75ee5a012c2af5d8326008
preetisilla <preetisilla18@gmail.com>
2024-09-24 18:29:25 +0530

    New Project

---
+++ src/Sept/ex_13092024/Lab016.java
@@ -0,0 +1,24 @@
+package Sept.ex_13092024;
+
+public class Lab016 {
+    public static void main(String[] args) {
+        //Literal
+        //boolean literal
+        boolean is_married = true; // false
+
+        // integral literal
+        int age = 78;
+        int binary = 0b1010; //binary
+        int octal = 0101; // octal
+        int hex = 0Xface; // hexadecimal
+
+        //character literal
+        char c = 'A';
+        char c1 = '@';
+        char new_line = '\n'; // escape char
+        char tab_line =  '\t'; // b backspace, \r carriage return
+
+        System.out.println("Preeti" + new_line + "Dutta");
+        char c11 = '\u1F60';
+    }
+}
