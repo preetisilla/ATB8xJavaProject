4a50a4f414ab17bfd85cfa1044aa1d3581516d58
preetisilla <preetisilla18@gmail.com>
2024-09-25 21:13:19 +0530

    New Project

---
+++ src/Sept/ex_16092024/Lab020.java
@@ -0,0 +1,10 @@
+package Sept.ex_16092024;
+
+public class Lab020 {
+    public static void main(String[] args) {
+        short s = 10;
+        char c = 'A'; // ASCII value 65
+        int ss = s+c;
+        System.out.println(ss);
+    }
+}
